c75fc6919b0e5725fd9412ba180c3f841453e41d
riya-fathima-2005 <riyaf8052@gmail.com>
2025-12-01 20:30:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+async function getData() {
+    const data = await fetch('https://dummyjson.com/users ');
+    const records = await data.json();
+
+    let tab = '';
+
+    records.users.forEach(function (user) {
+        tab += `
+        <tr>
+            <td>${user.id}</td>
+            <td>${user.firstName}</td>
+            <td>${user.lastName}</td>
+            <td>${user.age}</td>
+            <td>${user.gender}</td>
+            <td>${user.email}</td>
+            <td>${user.phone}</td>
+        </tr>`;
+    });
+
+    document.getElementById('tbody').innerHTML = tab;
+}
